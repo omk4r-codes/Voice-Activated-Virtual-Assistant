c4409ec8972b726b851316949425d08417f18037
Omkar <omkarsalunke1978@gmail.com>
2023-10-04 22:32:09 +0530

    added songs list

--- namesofsites.py
+++ namesofsites.py
@@ -14,4 +14,13 @@ sites = [
     ["Netflix", "https://www.netflix.com"],
     ["Reddit", "https://www.reddit.com"],
     ["ChatGPT", "https://chat.openai.com"],
+    ["Spotify", "https://open.spotify.com"],
+]
+
+songs = [
+    ["On and On", "C:\\Users\\omkar\\OneDrive\\Desktop\\NCS_Songs\\Cartoon - On & On (feat. Daniel Levi) [NCS Release].mp3"],
+    ["Royalty", "C:\\Users\\omkar\\OneDrive\\Desktop\\NCS_Songs\\Egzod, Maestro Chives, Neoni - Royalty [NCS Release].mp3"],
+    ["Heroes Tonight", "C:\\Users\\omkar\\OneDrive\\Desktop\\NCS_Songs\\Janji - Heroes Tonight (feat. Johnning) [NCS Release].mp3"],
+    ["Mortals", "C:\\Users\\omkar\\OneDrive\\Desktop\\NCS_Songs\\Warriyo - Mortals (feat. Laura Brehm) [NCS Release].mp3"],
+    ["Fearless", "C:\\Users\\omkar\\OneDrive\\Desktop\\NCS_Songs\\TULE - Fearless pt.II (feat. Chris Linton) [NCS Release].mp3"]
 ]
